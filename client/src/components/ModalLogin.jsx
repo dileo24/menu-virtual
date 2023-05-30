8f26600c3e6babe86dd6dd8f38cc4a320e76e849
Lautaro <lautiquiroga10@gmail.com>
2023-05-30 13:47:22 -0300

    iniciar sesion con tailwind

--- client/src/components/ModalLogin.jsx
+++ client/src/components/ModalLogin.jsx
@@ -24,43 +24,71 @@ export default function ModalLogin({ onClose }) {
   };
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        {/* <span className="close" onClick={onClose}>
-          &times;
-        </span> */}
-        <h2>Iniciar Sesión</h2>
-        <div className="form">
-          <form onSubmit={handleSubmit}>
-            <label>
-              Correo electrónico:
-              <input
-                type="email"
-                name="email"
-                value={input.email}
-                onChange={(e) => handlerChange(e)}
-                required
-              />
-            </label>
-            <label>
-              Contraseña:
-              <input
-                type="password"
-                name="clave"
-                value={input.clave}
-                onChange={(e) => handlerChange(e)}
-                required
-              />
-            </label>
-            <div className="modal-footer">
-              <button type="submit">Iniciar Sesión</button>
-              <Link to="/">
-                <button type="button" onClick={onClose}>
-                  Cancelar
+    <div className="flex flex-col  justify-center h-screen bg-gray-200">
+      <h2 className="  -mt-16 text-3xl font-light text-center">
+        Iniciar Sesión
+      </h2>
+
+      <div className="flex flex-col mt-10 items-center contenedor w-full">
+        <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 w-10/12 md:w-8/12 lg:w-6/12">
+          {/* <span className="close" onClick={onClose}>
+            &times;
+          </span> */}
+          <div className="form shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+            <form onSubmit={handleSubmit} className="bg-white p-3">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Correo electrónico
+                </label>
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="email"
+                  name="email"
+                  placeholder="Escribe tu email"
+                  value={input.email}
+                  onChange={(e) => handlerChange(e)}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="clave"
+                >
+                  Contraseña
+                </label>
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="password"
+                  name="clave"
+                  placeholder="Escribe tu contraseña"
+                  value={input.clave}
+                  onChange={(e) => handlerChange(e)}
+                  required
+                />
+              </div>
+              <div className="modal-footer flex w-full justify-between">
+                <button
+                  type="submit"
+                  className="rounded w-56 bg-teal-600 hover:bg-teal-900 ml-20 mt-5 p-2 text-white uppercase font-bold cursor-pointer"
+                >
+                  Iniciar Sesión
                 </button>
-              </Link>
-            </div>
-          </form>
+                <Link to="/">
+                  <button
+                    type="button"
+                    className="rounded bg-red-700 hover:bg-red-900 mr-20 w-56 mt-5 p-2 text-white uppercase font-bold cursor-pointer"
+                    onClick={onClose}
+                  >
+                    Cancelar
+                  </button>
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
