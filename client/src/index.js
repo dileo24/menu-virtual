c47535d2059825965235e2c3ce6fbd15e8b9234c
Lautaro <lautiquiroga10@gmail.com>
2023-05-30 16:15:28 -0300

    instalé scss

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-// import "../src/styles/styles.scss";
+import "../src/styles/styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
