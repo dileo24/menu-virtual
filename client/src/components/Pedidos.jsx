2cb8fc535f0d69bccb7f1cd2611f6cef5aa64d7f
Joaquin <joacodileo@gmail.com>
2023-06-02 18:15:55 -0300

    listado de pedidos

--- client/src/components/Pedidos.jsx
+++ client/src/components/Pedidos.jsx
@@ -1,13 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Aside from "./Aside";
+import { useDispatch, useSelector } from "react-redux";
+import Filtros from "./Filtros";
+import { getPedidos } from "../redux/actions";
 
 export default function Pedidos() {
+  const pedidos = useSelector((state) => state.pedidos);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPedidos());
+  }, [dispatch]);
+
   return (
-    <div className="min-h-100 bg-gray-200">
-      <div className="md:flex min-h-screen md:align-top">
-        <Aside />
-        <h2 className="text-3xl font-light text-center">Planilla de pedidos</h2>
+    pedidos && (
+      <div id="productos" className="min-h-100 bg-gray-200">
+        <div className="md:flex min-h-screen md:align-top">
+          <Aside />
+          <main className="md:w-4/5 xl:w-4/5  py-10 bg-gray-200">
+            <h2 className="text-3xl font-light text-center">
+              Planilla de Pedidos
+            </h2>
+            <div className="px-5 flex flex-col mt-10">
+              <Filtros />
+              <div className="py-2 overflow-x-auto">
+                <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                  <table className="min-w-full">
+                    <thead className="bg-gray-100">
+                      <tr>
+                        <th className="px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                          Productos
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                          Mesa
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                          Precio
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                          Estado
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white">
+                      {pedidos.map(
+                        ({ productos, mesa, precio, Estado, id }) => (
+                          <tr key={id}>
+                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                              <p className="text-sm leading-5 font-medium text-gray-700 text-lg font-bold">
+                                {productos}
+                              </p>
+                            </td>
+                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                              <p className="text-gray-700">{mesa}</p>
+                            </td>
+                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 leading-5 text-gray-700">
+                              <p className="text-gray-600">${precio}</p>
+                            </td>
+                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                              {Estado.tipo}
+                            </td>
+                          </tr>
+                        )
+                      )}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
+    )
   );
 }
