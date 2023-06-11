2e82d3e68c9d6b3392122252368dbce0b1e0d891
Lautaro <lautiquiroga10@gmail.com>
2023-06-11 20:39:27 -0300

    mejor tabla en pedidosCliente

--- client/src/components/secciones/PedidosCliente.jsx
+++ client/src/components/secciones/PedidosCliente.jsx
@@ -49,41 +49,38 @@ export default function Pedidos() {
             <div className="px-5 flex flex-col mt-10">
               {/* <Filtros /> */}
               <div className="py-2 overflow-x-auto">
-                <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
-                  <table className="text-center">
-                    <thead>
+                <div className="align-middle inline-block min-w-full overflow-hidden sm:rounded-lg flex justify-center">
+                  <table className="text-center shadow-b">
+                    <thead className="bg-gray-300">
                       <tr>
-                        <th className="text-center px-4 py-2">Fecha</th>
-                        <th className="text-center px-4 py-2">Hora</th>
-                        <th className="text-center px-4 py-2">Nombre</th>
+                        <th className="text-center px-4 py-2">Pedidos</th>
                         <th className="text-center px-4 py-2">Precio</th>
-                        <th className="text-center px-4 py-2">
-                          Método de Pago
-                        </th>
                         <th className="text-center px-4 py-2">Estado</th>
                       </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="bg-white rounded ">
                       {inputData.map((input) => (
-                        <tr>
-                          <td className="text-center px-4 py-2">
-                            {input.creacionFecha}
+                        <tr className=" border-b-2 not:last-child">
+                          <td className="text-left px-10 py-5">
+                            <p className="text-gray-700 mt-2">
+                              <b>{input.productos.join(", ")}</b>
+                            </p>
+                            <p className="text-gray-700 mt-2">
+                              <b>Fecha: </b>
+                              {input.creacionFecha} <b>Hora: </b>
+                              {input.creacionHora}
+                            </p>
                           </td>
-                          <td className="text-center px-4 py-2">
-                            {input.creacionHora}
+                          <td className="text-center px-10 py-2">
+                            ${input.precio}
+                            <p className="text-gray-700 mt-2">
+                              Método de Pago:{" "}
+                              <b>{getTipoPagoNombre(input.tipoPagoID)}</b>
+                            </p>
                           </td>
-                          <td className="text-center px-4 py-2">
-                            {input.productos.join(", ")}
-                          </td>
-                          <td className="text-center px-4 py-2">
-                            {input.precio}
-                          </td>
-                          {/* <td>{getTipoPago(input.tipoPagoID)}</td> */}
-                          <td className="text-center px-4 py-2">
-                            {getTipoPagoNombre(input.tipoPagoID)}
-                          </td>
-                          <td className="text-center px-4 py-2">
+                          <td className="text-center px-10 py-2">
                             {/* estado del pedido */}
+                            Entregado
                           </td>
                         </tr>
                       ))}
