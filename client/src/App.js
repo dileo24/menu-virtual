7bfa59bcc869f1679ed08ab370b2479b432669e4
Joaquin <joacodileo@gmail.com>
2023-07-06 18:49:04 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -16,10 +16,10 @@ import UpdateItemsCliente from "./components/secciones/UpdateItemsCliente";
 import NuevaCateg from "./components/formularios/NuevaCateg";
 
 // Local
-axios.defaults.baseURL = "http://localhost:3001";
-
+/* axios.defaults.baseURL = "http://localhost:3001";
+ */
 // Deploy
-//axios.defaults.baseURL = "https://menu-virtual-production-9dbc.up.railway.app";
+axios.defaults.baseURL = "https://menu-virtual-production-9dbc.up.railway.app";
 
 function App() {
   const userActual = useSelector((state) => state.userActual);
