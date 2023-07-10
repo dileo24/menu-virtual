4d1043a15c4edf7e8c00be5d59daf2a8f28dd7b4
Joaquin <joacodileo@gmail.com>
2023-07-10 19:13:32 -0300

    creación sin subC

--- client/src/components/recursos/Filtros.jsx
+++ client/src/components/recursos/Filtros.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getCategorias, searchXname } from "../../redux/actions";
-// import { FaSistrix } from "react-icons/fa";
 
 export default function Filtros() {
   const [state, setState] = useState("");
@@ -38,23 +37,7 @@ export default function Filtros() {
           onKeyDown={handleKeyDown}
           value={state}
         />
-        {/* <button onClick={limpiarState}>
-          <FaSistrix className="lupa" />
-        </button> */}
       </div>
-      {/* <button style={{ backgroundColor: "grey" }} onClick={handleRecargar}>
-        Limpiar filtros/búsquedas
-      </button>
-      <select value={filtroCategoria} onChange={(e) => handleFilterCateg(e)}>
-        <option value="todas" hidden>
-          Todos los Productos
-        </option>
-        {categorias.map((categ) => (
-          <option key={categ.id} value={categ.nombre}>
-            {categ.nombre}
-          </option>
-        ))}
-      </select> */}
     </div>
   );
 }
