53aac5d7ed003863de54972cf7db1c94e574edde
Lautaro <lautiquiroga10@gmail.com>
2023-07-27 17:04:08 -0300

    fixes

--- client/src/components/formularios/Items.jsx
+++ client/src/components/formularios/Items.jsx
@@ -1,105 +1,3 @@
-// import React from "react";
-// import Button from "../recursos/Button";
-// import { useSelector } from "react-redux";
-
-// export default function Items({
-//   itemsExtra,
-//   setItemsExtra,
-//   numItemsExtra,
-//   setNumItemsExtra,
-//   itemsExtraArray,
-//   categoriaID,
-// }) {
-//   const categorias = useSelector((state) => state.categorias);
-//   const categsItems = categorias.filter((c) => c.id !== 1 && c.id !== 2);
-
-//   const handleNumItemsChange = (e) => {
-//     let count = parseInt(e.target.value);
-//     setNumItemsExtra(count);
-//     setItemsExtra(Array(count).fill(""));
-//   };
-
-//   const handleItemChange = (e, index) => {
-//     const updatedItems = [...itemsExtra];
-//     updatedItems[index] = e.target.value;
-//     setItemsExtra(updatedItems);
-//   };
-
-//   const incrementNumItems = () => {
-//     let newNumItemsExtra = numItemsExtra;
-//     newNumItemsExtra++;
-//     setNumItemsExtra(newNumItemsExtra);
-//     let newItemsExtra = [...itemsExtra];
-//     for (let i = 0; i < newNumItemsExtra - itemsExtra.length; i++) {
-//       newItemsExtra.push("");
-//     }
-//     setItemsExtra(newItemsExtra);
-//   };
-
-//   const decrementNumItems = () => {
-//     if (numItemsExtra > 0) {
-//       setNumItemsExtra(numItemsExtra - 1);
-//       setItemsExtra(itemsExtra.slice(0, -1));
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div className="mb-4">
-//         <label
-//           className="block text-gray-700 text-sm font-bold mb-2"
-//           htmlFor="numItemsExtra"
-//         >
-//           Ítems Extra
-//           <span className="font-normal"> (no obligatorio)</span>
-//         </label>
-//         <div className="flex">
-//           {numItemsExtra ? (
-//             <>
-//               {" "}
-//               <Button signo="-" funcion={decrementNumItems} />
-//               <p
-//                 className="w-5 mx-3 text-center text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-//                 onChange={handleNumItemsChange}
-//                 readOnly
-//               >
-//                 {numItemsExtra}
-//               </p>
-//             </>
-//           ) : (
-//             ""
-//           )}
-
-//           <Button signo="+" funcion={incrementNumItems} />
-//         </div>
-//       </div>
-
-//       {itemsExtra.map((item, index) => (
-//         <div className="mb-4" key={index}>
-//           <label
-//             className="block text-gray-700 text-sm font-bold mb-2"
-//             htmlFor={`item${index}`}
-//           >
-//             ítem {index + 1}
-//           </label>
-//           <select onChange={(e) => handleItemChange(e, index)}>
-//             <option hidden>
-//               {itemsExtra[index] === "" ? "Elegí un item" : itemsExtra[index]}
-//             </option>
-
-//             {categsItems.map((item) => (
-//               <option key={item.id} value={item.nombre}>
-//                 {item.nombre}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
-// Código para también imprimir los nombres de las subcategorías que contengan productos con item:true
 import React from "react";
 import Button from "../recursos/Button";
 import { useSelector } from "react-redux";
@@ -149,26 +47,7 @@ export default function Items({
     }
   };
 
-  // Filter productosConItemTrue to store only unique items based on names
-  // const uniqueProductosConItemTrue = Array.from(
-  //   new Set(
-  //     productosConItemTrue.map((producto) => {
-  //       return producto.subcategoria
-  //         ? producto.subcategoria.nombre
-  //         : producto.categoria.nombre;
-  //     })
-  //   )
-  // ).map((nombre) => {
-  //   return productosConItemTrue.find((producto) => {
-  //     return (
-  //       (producto.subcategoria
-  //         ? producto.subcategoria.nombre
-  //         : producto.categoria.nombre) === nombre
-  //     );
-  //   });
-  // });
-
-  const categoriasUnicas = Array.from(
+  /* const categoriasUnicas = Array.from(
     new Set(
       productosConItemTrue.map((producto) => {
         return producto.subcategoria
@@ -176,7 +55,7 @@ export default function Items({
           : producto.categoria.nombre;
       })
     )
-  );
+  ); */
 
   return (
     <>
@@ -222,11 +101,45 @@ export default function Items({
               {itemsExtra[index] === "" ? "Elegí un item" : itemsExtra[index]}
             </option>
 
-            {categoriasUnicas.map((nombre, index) => (
+            {/* {categoriasUnicas.map((nombre, index) => (
               <option key={index} value={nombre}>
                 {nombre}
               </option>
-            ))}
+            ))} */}
+
+            {/* Mapear las opciones de categorías */}
+            {categorias.map((categoria) => {
+              const categoriaEstaEnProductos = productosConItemTrue.some(
+                (producto) => producto.categoria.id === categoria.id
+              );
+
+              if (categoriaEstaEnProductos) {
+                return (
+                  <option key={categoria.id} value={categoria.nombre}>
+                    {categoria.nombre}
+                  </option>
+                );
+              }
+              return null;
+            })}
+
+            {/* Mapear las opciones de subcategorías */}
+            {subcategorias.map((subcategoria) => {
+              const subcategoriaEstaEnProductos = productosConItemTrue.some(
+                (producto) =>
+                  producto.subcategoria &&
+                  producto.subcategoria.id === subcategoria.id
+              );
+
+              if (subcategoriaEstaEnProductos) {
+                return (
+                  <option key={subcategoria.id} value={subcategoria.nombre}>
+                    {subcategoria.nombre}
+                  </option>
+                );
+              }
+              return null;
+            })}
           </select>
         </div>
       ))}
