641deb74f97fe21b7a7f5e19fd28c0a1c8ff02d3
Joaquin <joacodileo@gmail.com>
2023-07-30 22:05:39 -0300

    empezando estilos mis pedidos

--- client/src/components/secciones/Historial.jsx
+++ client/src/components/secciones/Historial.jsx
@@ -4,6 +4,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPedidos } from "../../redux/actions";
 import io from "socket.io-client";
 import HeaderBack from "../recursos/HeaderBack";
+import {
+  BsCreditCardFill,
+  BsCashStack,
+  BsCashCoin,
+  BsCheckLg,
+  BsClock,
+} from "react-icons/bs";
+import { SiMercadopago } from "react-icons/si";
+import { TbBrandCashapp } from "react-icons/tb";
+import { GiCook } from "react-icons/gi";
 
 export default function Historial() {
   const pedidos = useSelector((state) => state.pedidos);
@@ -55,10 +65,46 @@ export default function Historial() {
   let pedidosActuales = inputData.map((idPed) =>
     pedidos.filter((ped) => ped.id === idPed.id)
   );
-
-  /* console.log(pedidosActuales);
-  console.log(inputData);
-  console.log(pedidos); */
+  const iconPago = (pagoId) => {
+    switch (pagoId) {
+      case 1:
+        return <BsCashCoin />;
+      case 2:
+        return <BsCashStack />;
+      case 3:
+        return <BsCreditCardFill />;
+      case 4:
+        return <SiMercadopago />;
+      case 5:
+        return <TbBrandCashapp />;
+      default:
+        return null;
+    }
+  };
+  const iconEstado = (estadoId) => {
+    switch (estadoId) {
+      case 1:
+        return <BsClock />;
+      case 2:
+        return <GiCook />;
+      case 3:
+        return <BsCheckLg />;
+      default:
+        return null;
+    }
+  };
+  const getClassForEstado = (estadoId) => {
+    switch (estadoId) {
+      case 1:
+        return "estado-info";
+      case 2:
+        return "estado-naranja";
+      case 3:
+        return "estado-success";
+      default:
+        return "estado-info";
+    }
+  };
 
   return pedidos &&
     pedidosActuales.length > 0 &&
@@ -72,47 +118,61 @@ export default function Historial() {
         arrowType={"left"}
         title={`Mis Pedidos Realizados`}
       />
-      {
-        pedidosActuales
-          .filter(
-            (pedido) =>
-              pedido[0] &&
-              pedido[0].Estado.id !== 4 &&
-              pedido[0].Estado.id !== 5
-          )
-          .map(
-            (pedido, index) =>
-              pedido[0] && (
-                <div className="cardPedido" key={index}>
-                  <div className="nombreItemsPrecio">
-                    <div className="nombreItems">
-                      {pedido[0].productos.map((producto, i) => (
-                        <div key={i}>
-                          <p className="nombre">{producto}</p>
-                          {pedido[0].itemsExtra[i] &&
-                            pedido[0].itemsExtra[i].length > 0 && (
-                              <ul className="itemsExtra">
-                                {pedido[0].itemsExtra[i].map((item, j) => (
-                                  <li key={j} className="list-item">
-                                    <span className="list-item-circle"></span>
-                                    {item}
-                                  </li>
-                                ))}
-                              </ul>
-                            )}
-                        </div>
-                      ))}
-                    </div>
+      {pedidosActuales
+        .filter(
+          (pedido) =>
+            pedido[0] && pedido[0].Estado.id !== 4 && pedido[0].Estado.id !== 5
+        )
+        .map(
+          (pedido, index) =>
+            pedido[0] && (
+              <div className="cardPedido" key={index}>
+                <div className="nombreItemsPrecio">
+                  <div className="supBar">
+                    <p className="estado-info">Mesa {pedido[0].mesa}</p>
+                    <p
+                      className={`estado ${getClassForEstado(
+                        pedido[0].Estado.id
+                      )}`}
+                    >
+                      {iconEstado(pedido[0].Estado.id)}
+                      <span style={{ marginLeft: "5px" }}>
+                        {pedido[0].Estado.tipo}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="nombreItems">
+                    {pedido[0].productos.map((producto, i) => (
+                      <div key={i}>
+                        <p className="nombre">{producto}</p>
+                        {pedido[0].itemsExtra[i] &&
+                          pedido[0].itemsExtra[i].length > 0 && (
+                            <ul className="itemsExtra">
+                              {pedido[0].itemsExtra[i].map((item, j) => (
+                                <li key={j} className="list-item">
+                                  <span className="list-item-circle"></span>
+                                  {item}
+                                </li>
+                              ))}
+                            </ul>
+                          )}
+                      </div>
+                    ))}
+                  </div>
+                  <div className="footerPed">
+                    <p className="metodoDePago estado-success">
+                      {iconPago(pedido[0].Pago.id)}
+                      <span style={{ marginLeft: "5px" }}>
+                        {pedido[0].Pago.tipo}
+                      </span>
+                    </p>
+                    <p className="total">Total: </p>
                     <p className="precio">${pedido[0].precio}</p>
                   </div>
-
-                  {/* <p className="metodoDePago">{pedido[0].Pago.tipo}</p> */}
-                  <p className="estado">{pedido[0].Estado.tipo}</p>
                 </div>
-              )
-          )
-        /* .reverse() */
-      }
+              </div>
+            )
+        )}
     </div>
   ) : (
     <div className="historialContainer">
